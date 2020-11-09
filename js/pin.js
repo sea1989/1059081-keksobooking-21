47e202b520fb2d1eb3492ced19fc3249ecadfafe
Victor <vicvilkov@yandex.ru>
2020-11-09 23:17:08 +0300

    Личный проект: перламутровые пуговицы (часть 2)

--- js/pin.js
+++ js/pin.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 
 (function activate() {
   // отрисовка пинов
 
-  var similarListElement = document.querySelector(".map__pins");
+  var similarListElement = document.querySelector('.map__pins');
   var similarPinTemplate = document
-    .querySelector("#pin")
-    .content.querySelector(".map__pin");
+    .querySelector('#pin')
+    .content.querySelector('.map__pin');
 
   var createPinsBlock = function (data) {
     var fragment = document.createDocumentFragment();
@@ -17,23 +17,23 @@
       .forEach(function (pin) {
         var pinsElement = similarPinTemplate.cloneNode(true);
 
-        pinsElement.querySelector("img").src = pin.author.avatar;
-        pinsElement.querySelector("img").alt = pin.offer.title;
+        pinsElement.querySelector('img').src = pin.author.avatar;
+        pinsElement.querySelector('img').alt = pin.offer.title;
         pinsElement.style =
-          "left: " +
+          'left: ' +
           (pin.location.x - 25) +
-          "px; top: " +
+          'px; top: ' +
           (pin.location.y - 70) +
-          "px;";
+          'px;';
 
-        pinsElement.addEventListener("click", function (evt) {
-          var pinActive = document.querySelector(".map__pin--active");
+        pinsElement.addEventListener('click', function (evt) {
+          var pinActive = document.querySelector('.map__pin--active');
 
           if (pinActive) {
-            pinActive.classList.remove("map__pin--active");
+            pinActive.classList.remove('map__pin--active');
           }
 
-          pinsElement.classList.add("map__pin--active");
+          pinsElement.classList.add('map__pin--active');
 
           if (!evt.button) {
             window.createCardAdverts(pin);
--- js/reset.js
+++ js/reset.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
 (function activate() {
-  var form = document.querySelector(".ad-form");
-  var formFieldsets = form.querySelectorAll("fieldset");
-  var buttonReset = document.querySelector(".ad-form__reset");
+  var form = document.querySelector('.ad-form');
+  var formFieldsets = form.querySelectorAll('fieldset');
+  var buttonReset = document.querySelector('.ad-form__reset');
 
   // переключение fieldset
 
@@ -16,7 +16,7 @@
   // функция ДЕактивации страницы
 
   var deactivatePage = function () {
-    document.querySelector(".map").classList.add("map--faded");
+    document.querySelector('.map').classList.add('map--faded');
     switchFieldset(formFieldsets, true);
     window.removePopups();
     window.loadResult = null;
@@ -29,21 +29,21 @@
 
   // ДЕактивирует при нажатии на мышку
 
-  buttonReset.addEventListener("mousedown", function (evt) {
+  buttonReset.addEventListener('mousedown', function (evt) {
     if (!evt.button) {
       deactivatePage();
 
-      form.classList.add("ad-form--disabled");
+      form.classList.add('ad-form--disabled');
     }
   });
 
   // ДЕактивирует при нажатии на enter
 
-  buttonReset.addEventListener("keydown", function (evt) {
-    if (evt.key === "Enter") {
+  buttonReset.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
       deactivatePage();
 
-      form.classList.add("ad-form--disabled");
+      form.classList.add('ad-form--disabled');
     }
   });
 })();
